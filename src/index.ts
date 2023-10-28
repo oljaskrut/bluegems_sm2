c5a36aae1f8fc308645bfaa01d98add2f414cf1d
oljaskrut <oljaskrutf@gmail.com>
2023-10-28 16:25:56 +0600

    error messages

--- src/index.ts
+++ src/index.ts
@@ -8,8 +8,8 @@ app.listen(3000, () => {
 
 app.get("/", async (req, res) => {
   const cred = await credo()
-  if (!cred) return res.status(500)
+  if (!cred) return res.status(500).json({ msg: "failed to get xcsrf token" })
   const data = await ff(0, cred.sm_id, cred.csrfToken)
-
+  if (!data) return res.status(500).json({ msg: "failed to get items" })
   res.status(200).json(data)
 })
