832a6f01e99b9fff28e4b9cc4300fad04428e926
oljaskrut <oljaskrutf@gmail.com>
2023-10-28 16:29:59 +0600

    port specified

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,9 @@ import express from "express"
 import { credo, ff } from "./fetcher.js"
 
 const app = express()
-app.listen(3000, () => {
-  console.log("Server started on port 3000")
+const PORT = process.env.PORT || 8080
+app.listen(PORT, () => {
+  console.log("Server started on port " + PORT)
 })
 
 app.get("/", async (req, res) => {
